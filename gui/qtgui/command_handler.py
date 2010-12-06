2c2eb88656566ab9e2c18ffc888c99498974d2b9
sdjakl <sdjakl@fail0verflow.com>
2010-12-05 23:33:30 -0800

    add 'a' add binary command

--- gui/qtgui/command_handler.py
+++ gui/qtgui/command_handler.py
@@ -11,7 +11,46 @@ from PySide import QtGui
 
 keyList = dict([(getattr(QtCore.Qt, key), key) for key in dir(QtCore.Qt) if key.startswith('Key')])
 
+class AddBinaryPromptWindow(QtGui.QDialog):
+	def __init__(self):
+		super(AddBinaryPromptWindow, self).__init__()
+		
+		# Todo - make validator that changes background red for bad values
+		# and disables okButton 
+		
+		# TODO: add cancel button
+		
+		# TODO: remove close and make act like a normal dialog
+		okButton = QtGui.QPushButton("OK")
+		okButton.setDefault(True)
+		QtCore.QObject.connect(okButton, QtCore.SIGNAL('clicked()'), self.accept)
+
+		buttonBox = QtGui.QDialogButtonBox(QtCore.Qt.Vertical)
+		buttonBox.addButton(okButton, QtGui.QDialogButtonBox.ActionRole)
+
+		self.formLayout = QtGui.QFormLayout()
+		self.baseEdit = QtGui.QLineEdit("0x0")
+		self.startEdit = QtGui.QLineEdit("0x0")
+		self.lengthEdit = QtGui.QLineEdit("-1")
+		self.formLayout.addRow("&Base Address:", self.baseEdit)
+		self.formLayout.addRow("&Start Offset:", self.startEdit)
+		self.formLayout.addRow("&Length:", self.lengthEdit)
+		self.formLayout.addWidget(buttonBox)
+		self.setLayout(self.formLayout)
+		self.setWindowModality(QtCore.Qt.ApplicationModal)
+		
 class CommandHandler(object):
+
+	def handleAddBinary(self, addr):
+		filename, filter = QtGui.QFileDialog.getOpenFileName()
+		bpw = AddBinaryPromptWindow()
+		bpw.exec_()
+		base_addr = int(bpw.baseEdit.text(),0)
+		start_offset = int(bpw.startEdit.text(),0)
+		length = int(bpw.lengthEdit.text(),0)
+		idis.tools_loaders.addBinary(self.ds, filename, base_addr, start_offset, length)
+		
+		
 	def handleInspect(self, addr):
 		info = self.ds[addr]
 		iw = InspectWindow(info)
@@ -25,7 +64,6 @@ class CommandHandler(object):
 			self.ds[addr].label = text
 
 
-		
 	def handleCodeFollow(self, addr):
 		# TODO Hack
 		a = arch.architectureFactory('8051')
@@ -59,6 +97,7 @@ class CommandHandler(object):
 		self.memstack = []
 		self.view = view
 		self.cmd_handlers = {
+			QtCore.Qt.Key_A: self.handleAddBinary,
 			QtCore.Qt.Key_I: self.handleInspect,
 			QtCore.Qt.Key_C: self.handleCodeFollow,
 			QtCore.Qt.Key_Return: self.handleFollowJump,
